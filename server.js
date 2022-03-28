07546e52615cce291e463e7ec541cc761ac8d947
Shangjie Zhou <zhoujay73@gmail.com>
2022-03-28 23:40:16 +0800

    using express

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const io = require("socket.io")(
-    3000,
-    {
-        cors: {
-            origin: ["https://lifeboat-client-0gddwfowd10071bd-1302413344.ap-shanghai.app.tcloudbase.com/"]
-        }
-    }
-);
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
-io.on("connection", socket => {
-    console.log("++++++++++")
-    console.log(socket.id);
-    console.log("---------")
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "https://lifeboat-client-0gddwfowd10071bd-1302413344.ap-shanghai.app.tcloudbase.com/",
+        methods: ["GET", "POST"],
+    }
 });
+
+io.on("connection", (client) => {
+    console.log(client.id);
+
+    client.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(3001, () => {
+    console.log("Server running")
+})
