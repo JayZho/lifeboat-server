dae518f1b0bc1a227f4a3ffcd131d8a0938c23c1
Shangjie Zhou <zhoujay73@gmail.com>
2022-04-06 17:51:29 +0800

    haha

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+        origin: [
+            "https://lifeboat-client-0gddwfowd10071bd-1302413344.ap-shanghai.app.tcloudbase.com",
+            "http://localhost:3000",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"]
     }
 });
 
 io.on('connection', socket => {
+    console.log(socket.id);
     io.emit('message', "server's greetings!");
 });
 
